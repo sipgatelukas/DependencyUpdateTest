bb9a2226bd0ea6dedf9d89131730fca1bc2c5e72
Lukas Lehmann <lehmann@sipgate.de>
2022-02-16 15:27:49 +0100

    Initial commit
    Co-authored-by: Florian Mai <mai@sipgate.de>
    Co-authored-by: Johannes Waigel <waigel@sipgate.de>

---
+++ src/main/java/com/example/dependencyupdatetest/DependencyUpdateTestApplication.java
@@ -0,0 +1,21 @@
+package com.example.dependencyupdatetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DependencyUpdateTestApplication {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyUpdateTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dependencyupdatetest/DependencyUpdateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencyupdatetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyUpdateTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
